0d9888a57e88f1b94cc6bf9fcb9739bb84041f8d
HackerU <Hackeru@Teacher.local>
2016-04-21 14:02:48 +0300

    added db

---
+++ routes/db.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE notes(id INT, title TEXT, note TEXT); 
+
+INSERT INTO notes VALUES(1, 'First Note', 'Call mom!');
+INSERT INTO notes VALUES(2, '2nd Note', 'Eat pitot...');
+INSERT INTO notes VALUES(3, '3rd Note', 'Watch marko!');
+INSERT INTO notes VALUES(4, '4th Note', 'Call daddy :)!');
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,44 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var pg = require('pg');
+
+var createDb = "CREATE TABLE notes(id INT, title TEXT, note TEXT);"
+createDb += "INSERT INTO notes VALUES(1, 'First Note', 'Call mom!');"
+createDb += "INSERT INTO notes VALUES(2, '2nd Note', 'Eat pitot...');"
+createDb += "INSERT INTO notes VALUES(3, '3rd Note', 'Watch marko!');"
+createDb += "INSERT INTO notes VALUES(4, '4th Note', 'Call daddy :)!');"
+
+
+
+router.get('/api/createdb', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query(createDb, function(err, result) {
+      done();
+      if (err){ console.error(err); response.send("Error " + err); }
+      else
+       { response.render('index', {title: result} ); }
+    });
+  });
+})
+
+
+
+
+router.get('/api/notes', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM notes', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else{ 
+      		var json = {};
+      		json.notes = result.rows;
+      		response.json(json);
+        }
+    });
+  });
+})
+
+
 module.exports = router;
